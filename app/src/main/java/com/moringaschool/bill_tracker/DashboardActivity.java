f6aed8aee57330ae3f3b51125214c910cff4b4fd
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-01-30 21:26:47 +0300

    creating an oncreate override method on dahsboardactivity

--- app/src/main/java/com/moringaschool/bill_tracker/DashboardActivity.java
+++ app/src/main/java/com/moringaschool/bill_tracker/DashboardActivity.java
@@ -1,6 +1,12 @@
 package com.moringaschool.bill_tracker;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity  extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
