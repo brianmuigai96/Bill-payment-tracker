baad3d386a0b643fba4fbc699568c07ea5d38e51
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-01-31 01:29:48 +0300

    adding a an input time to the password

--- app/src/main/java/com/moringaschool/bill_tracker/MainActivity.java
+++ app/src/main/java/com/moringaschool/bill_tracker/MainActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
     private EditText mUsername;
     private EditText mPassword;
+    private MaterialButton mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUsername =(EditText) findViewById(R.id.username);
+        mPassword = (EditText) findViewById(R.id.password);
+        mLoginButton =(MaterialButton) findViewById(R.id.loginbutton);
     }
 }
