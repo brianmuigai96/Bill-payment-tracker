7704d7ee01357bbb3dabaa7d85e8520063c42747
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-01-31 01:37:31 +0300

    adding butterknife dependencies

--- app/src/main/java/com/moringaschool/bill_tracker/MainActivity.java
+++ app/src/main/java/com/moringaschool/bill_tracker/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mPassword;
     private MaterialButton mLoginButton;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
