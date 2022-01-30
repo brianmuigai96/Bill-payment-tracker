bfa1f4f89080e5d190519190c3317c405f9609c3
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-01-31 01:18:58 +0300

    creating a username and password constractor

--- app/src/main/java/com/moringaschool/bill_tracker/MainActivity.java
+++ app/src/main/java/com/moringaschool/bill_tracker/MainActivity.java
@@ -3,8 +3,11 @@ package com.moringaschool.bill_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mUsername;
+    private EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
