5297e89668a29dd81604e4e86c4444a0746ef192
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-01-31 03:27:40 +0300

    creating constractors for custom array adapter

--- app/src/main/java/com/moringaschool/bill_tracker/DashboardArrayAdpater.java
+++ app/src/main/java/com/moringaschool/bill_tracker/DashboardArrayAdpater.java
@@ -1,2 +1,11 @@
-package com.moringaschool.bill_tracker;public class DashboardArrayAdpater {
+package com.moringaschool.bill_tracker;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class DashboardArrayAdpater extends ArrayAdapter {
+    private Context mContext;
+    private String  [] mservice;
+    private String [] mstatus;
+
 }
