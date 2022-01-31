918ab43ce5fe40890c8d4c7b4d982abf62b59ef9
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-01-31 03:58:41 +0300

    declaring arrays

--- app/src/main/java/com/moringaschool/bill_tracker/DashboardActivity.java
+++ app/src/main/java/com/moringaschool/bill_tracker/DashboardActivity.java
@@ -2,16 +2,25 @@ package com.moringaschool.bill_tracker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DashboardActivity  extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class DashboardActivity  extends AppCompatActivity {
+    @BindView(R.id.listView) ListView mListView;
+    private TextView UsernameEditText;
+    private String [] service = new String[]{"Dstv","Gotv","Rent","Car Insuarance","Water bill","Electricity bill"};
+    private String [] status = new String[]{"Paid","Pending","Unpaid","Overdue","Unpaid","Overdue"};
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         Intent intent = getIntent();
+        ButterKnife.bind(this);
         String payment = intent.getStringExtra("payment");
 
     }
