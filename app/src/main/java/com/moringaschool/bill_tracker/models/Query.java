8053c53c6c9851551336b040f81fdc4c35875a19
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-02-07 00:19:56 +0300

    adding jsonpojo to the model package

---
+++ app/src/main/java/com/moringaschool/bill_tracker/models/Query.java
@@ -0,0 +1,52 @@
+
+package com.moringaschool.bill_tracker;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Query {
+
+    @SerializedName("timestamp")
+    @Expose
+    private Integer timestamp;
+    @SerializedName("base_currency")
+    @Expose
+    private String baseCurrency;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Query() {
+    }
+
+    /**
+     * 
+     * @param baseCurrency
+     * @param timestamp
+     */
+    public Query(Integer timestamp, String baseCurrency) {
+        super();
+        this.timestamp = timestamp;
+        this.baseCurrency = baseCurrency;
+    }
+
+    public Integer getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Integer timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public void setBaseCurrency(String baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+}
