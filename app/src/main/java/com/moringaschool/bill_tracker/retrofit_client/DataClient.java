d687bc3cc49267a55b2159392f9070758797a0d0
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-02-07 10:11:01 +0300

    creating data client

--- app/src/main/java/com/moringaschool/bill_tracker/retrofit_client/DataClient.java
+++ app/src/main/java/com/moringaschool/bill_tracker/retrofit_client/DataClient.java
@@ -1,5 +1,33 @@
 package com.moringaschool.bill_tracker.retrofit_client;
 
-public class DataClient {
+import com.moringaschool.bill_tracker.network.ConvertApi;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class DataClient {
+    private static Retrofit retrofit = null;
+
+    public static ConvertApi getClient{
+        if(retrofit == null){
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest  = chain.request().newBuilder()
+                                    .addHeader("API_KEY")
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
+
+             retrofit = new Retrofit.Builder()
+                    .baseUrl("https://freecurrencyapi.net/api/v2/")
+                     .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            return retrofit.ConvertApi.class);
+        }
+    }
 }
