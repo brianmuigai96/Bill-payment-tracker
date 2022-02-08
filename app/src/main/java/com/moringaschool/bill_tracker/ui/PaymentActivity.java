c99abfd941316d8205c83cda8eea2531275f9cc3
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-02-08 22:54:47 +0300

    intializing the api

--- app/src/main/java/com/moringaschool/bill_tracker/ui/PaymentActivity.java
+++ app/src/main/java/com/moringaschool/bill_tracker/ui/PaymentActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.bill_tracker.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,17 +10,18 @@ import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.gson.JsonObject;
 import com.moringaschool.bill_tracker.Constants;
 
 import com.moringaschool.bill_tracker.R;
-import com.moringaschool.bill_tracker.models.Data;
+import com.moringaschool.bill_tracker.network.RetrofitInterface;
+import com.moringaschool.bill_tracker.retrofit_client.RetrofitBuilder;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class PaymentActivity  extends AppCompatActivity {
-    ConvertApi convertApi;
     Button button;
     EditText currencyToBeConverted;
     EditText currencyConverted;
@@ -47,20 +49,29 @@ public class PaymentActivity  extends AppCompatActivity {
         convertFromDropdown = (Spinner) findViewById(R.id.convert_from);
         button = (Button) findViewById(R.id.button);
 
-        convertApi = DataClient.getClient();
-        Call<Data> call =convertApi.getData(Constants.API_KEY);
-       call.enqueue(new Callback<Data>() {
-           @Override
-           public void onResponse(Call<Data> call, Response<Data> response) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RetrofitInterface retrofitInterface = RetrofitBuilder.getRetrofitInstance().create(RetrofitInterface.class);
+                Call<JsonObject> call = retrofitInterface.getExchangeCurrency(convertFromDropdown.getSelectedItem().toString());
+                call.enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                        JsonObject res = response.body();
+                        JsonObject rates = res.getAsJsonObject("rates");
+                        double currency = Double.valueOf(currencyToBeConverted.getText().toString());
+                        double multiplier = Double.valueOf(rates.get(convertToDropdown.getSelectedItem().toString()).toString());
+                        double result = currency * multiplier;
+                        currencyConverted.setText(String.valueOf(result));
+                    }
 
+                    @Override
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
 
-           }
-
-         @Override
-           public void onFailure(Call<Data> call, Throwable t) {
-
-           }
-       });
+                    }
+                });
+            }
+        });
 
 
     }
