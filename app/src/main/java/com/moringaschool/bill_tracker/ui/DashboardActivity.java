00b7de28541d542e65d6a8c03b1b61862188cef2
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-02-15 16:40:01 +0300

    validating registration credentials

--- app/src/main/java/com/moringaschool/bill_tracker/ui/DashboardActivity.java
+++ app/src/main/java/com/moringaschool/bill_tracker/ui/DashboardActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringaschool.bill_tracker.adapters.DashboardArrayAdpater;
 import com.moringaschool.bill_tracker.R;
 
@@ -76,5 +77,10 @@ public class DashboardActivity  extends AppCompatActivity implements  View.OnCli
     }
 
     private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(DashboardActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
