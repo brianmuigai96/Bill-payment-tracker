642df95cb3a6a89645a33c88806941e11e32bd65
brianmuigai96 <brian.muigai@student.moringaschool.com>
2022-02-15 16:05:02 +0300

    adding a logout method

--- app/src/main/java/com/moringaschool/bill_tracker/ui/DashboardActivity.java
+++ app/src/main/java/com/moringaschool/bill_tracker/ui/DashboardActivity.java
@@ -2,6 +2,9 @@ package com.moringaschool.bill_tracker.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -55,5 +58,23 @@ public class DashboardActivity  extends AppCompatActivity implements  View.OnCli
                 }
 
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
     }
 }
